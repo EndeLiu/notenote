498a0fe39a01cab925516f451532d41931eaffae
LiuTao <45730683+EndeLiu@users.noreply.github.com>
2020-03-18 21:41:11 +0800

    v0.0.1

---
+++ note_back/src/test/java/com/note/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
